de7e19d635e02ade8f1ece0e8e4c7ca013bb0c6a
Ana Gutiérrez Ruiz <ana.gutierrez.ruiz@Anas-iMac.local>
2023-02-02 10:10:29 +0100

    created Recipe Schema

--- models/Recipe.model.js
+++ models/Recipe.model.js
@@ -2,7 +2,40 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const recipeSchema = new Schema({
-  // TODO: write the schema
+  title: {
+    type: String,
+    required: "A title is required"
+  },
+  level: {
+    type: String,
+    enum: [ "Easy Peasy", "Amateur Chef", "Ultrapro Chef"]
+  },
+  ingredients: {
+    type: [ String ]
+  },
+  cuisine: {
+    type: String,
+    required: "A cuisine is required"
+  },
+  dishType: {
+    type: String,
+    enum: [ "breaskfast", "main-course", "soup", "snack", "drink", "dessert", "other"]
+  },
+  image: {
+    type: String,
+    default: "https://images.media-allrecipes.com/images/75131.jpg"
+  },
+  duration: {
+    type: Number,
+    minValue: 0
+  },
+  creator: {
+    type: String
+  },
+  created: {
+    type: Date,
+    default: Date.now
+  }
 });
 
 const Recipe = mongoose.model('Recipe', recipeSchema);
